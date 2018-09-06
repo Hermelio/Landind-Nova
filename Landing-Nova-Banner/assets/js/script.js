197ba3eb0a0e29c157d1dd4ed9fdc1b9c6a3aaaa
Hermelio <43037809+Hermelio@users.noreply.github.com>
2018-09-06 10:46:05 -0300

    Add files via upload

---
+++ Landing-Nova-Banner/assets/js/script.js
@@ -0,0 +1,274 @@
+
+var tradutor = {
+    "position":{
+        en:"You will soon join Multiplus!",
+        pt:"Em breve, você se unirá à Multiplus!",
+        es:"En breve, usted se unirá a Multiplus!",
+        dt:"Je zult snel lid worden van Multiplus!"
+    },
+    "position2":{
+        en:"Soon, your LATAM Airilines points will be concentrated on a single account at Mutiplus! Still do not know what Multiplus is? Well talk to you",
+        pt:"Em breve, seus pontos acumulados na LATAM Airilines serão concentrados em um única conta, na Mutiplus! Ainda não sabe o que é a Multiplus? A gente cona pra você",
+        es:"En breve, sus puntos acumulados en LATAM Airilines se concentrarán en una sola cuenta, en Mutiplus! ¿Todavía no sabe lo que es Multiplus? La gente cona para ti",
+        dt:"Binnenkort worden uw LATAM Airilines-punten geconcentreerd op één account bij Mutiplus! Weet je nog steeds niet wat Multiplus is? We zullen met je praten."
+    },
+    "position3":{
+        en:"Multiplus is the official LATAM Airilines partner in your country.",
+        pt:"A Multiplus é a parceira oficial da LATAM Airilines no seu pais.",
+        es:"Multiplus es el socio oficial de LATAM Airilines en su país.",
+        dt:"Multiplus is de officiële LATAM Airilines-partner in uw land."
+    },
+    "position4":{
+        en:"Being LATAM Pass and LATAM Fidelity automatically, you become a Multiplus participant.",
+        pt:"Ao ser LATAM Pass e LATAM Fidelidade, automaticamente, você se torna um participante Multiplus.",
+        es:"Al ser LATAM Pass y LATAM Fidelidad, automáticamente, usted se convierte en un participante Multiplus",
+        dt:"Als LATAM Pass en LATAM Fidelity wordt u automatisch Multiplus-deelnemer."
+    },
+    
+    "position5":{
+        en:"That's it! As such, your points and your flights will already contemplate your Multiplus balance, so that you can redeem worldwide passes in more than 1.00 destinations.",
+        pt:"Isso mesmo ! Sendo assim, seus pontos e seus voos ja contemplarão seu saldo Multiplus, para que você possa resgatar passagens pelo mundo, em mais de 1.00 destinos.",
+        es:"Eso mismo ! Siendo así, sus puntos y sus vuelos ya contemplarán su saldo Multiplus, para que usted pueda rescatar pasajes por el mundo, en más de 1.00 destinos.",
+        dt:"Dat is alles! Als zodanig zullen uw punten en uw vluchten al rekening houden met uw Multiplus-saldo, zodat u wereldwijd passen kunt inwisselen voor meer dan 1 bestemmingen."
+    },
+    
+    "position6":{
+        en:"Multiplus is the official LATAM Airilines partner in your country.",
+        pt:"Se você ja conhece a Multiplus , fique tranquilo. Seus benefícios aéreos continuarão os memsos.",
+        es:"Si usted ya conoce a Multiplus, quédese tranquilo. Sus beneficios aéreos continuarán los recuerdos.",
+        dt:"Als u Multiplus al kent, kunt u erop vertrouwen. Uw luchtvoordelen zullen de herinneringen voortzetten."
+    },
+    
+    "position7":{
+        en:"If you already know Multiplus, rest assured. Your aerial benefits will continue the memories.",
+        pt:"Se você ja conhece a Multiplus , fique tranquilo. Seus benefícios aéreos continuarão os memsos.",
+        es:"Si usted ya conoce a Multiplus, quédese tranquilo. Sus beneficios aéreos continuarán los recuerdos.",
+        dt:"Als u Multiplus al kent, kunt u erop vertrouwen. Uw luchtvoordelen zullen de herinneringen voortzetten."
+    },
+    "position8":{
+        en:"Why does this change occur?",
+        pt:"Por que ocorre essa mudança?",
+        es:"¿Por qué ocurre ese cambio?",
+        dt:"Waarom gebeurt deze verandering?"
+    },
+    "position9":{
+        en:"During the year 2018, the accumulation and redemption platforms will be unified in all countries where we operate. In your country will work the Multiplus platform, in which you will accumulate Multiplus points and redeem them for air tickets. Then learn more about this change.",
+        pt:"Durante o ano de 2018, as plataformas de acumulo e resgate serão unificadas em todos os países onde operamos. No seu país funcionará a plataforma Multiplus, em que você irá acumular pontos Multiplus e resgata-los por passagens aereas. A seguir saiba mais detalhes sobre essa mudança.",
+        es:"Durante el año 2018, las plataformas de acumulación y rescate serán unificadas en todos los países donde operamos. En su país funcionará la plataforma Multiplus, en la que usted acumular puntos Multiplus y rescatarlos por pasajes aéreos. A continuación se dan más detalles sobre este cambio.",
+        dt:"Gedurende het jaar 2018 zullen de accumulatie- en inkoopplatforms verenigd zijn in alle landen waar we actief zijn. In uw land zal het Multiplus-platform werken, waarin u Multiplus-punten verzamelt en inwisselt voor vliegtickets. Leer dan meer over deze verandering."
+    },
+    "position10":{
+        en:"Want to know more about change?",
+        pt:"Quer saber mais sobre a mudança?",
+        es:"¿Quieres saber más sobre el cambio?",
+        dt:"Wilt u meer weten over verandering?"
+    },
+    "position11":{
+        en:"Will this change affect my current balance?",
+        pt:"Essa alteracao afetará meu saldo atual?",
+        es:"¿Esta alteración afectará mi saldo actual?",
+        dt:"Zal deze verandering mijn huidige balans beïnvloeden?"
+    },
+    "position12":{
+        en:"No. Your LATAM Pass points will be renamed Multiplus points, but the balance in your account will not be affected by this change and will be available on the Multiplus platform so that you can exchange it for various products and services",
+        pt:"Não. Seus pontos LATAM Pass serão renomeados pontos Multiplus, mas o saldo em sua conta não será afetado por essa mudança e estará disponivel na plataforma da Multiplus, para que você possa trocá-lo por diversos produtos e serviços",
+        es:"No, sus puntos LATAM Pass serán renombrados puntos Multiplus, pero el saldo en su cuenta no será afectado por ese cambio y estará disponible en la plataforma de Multiplus, para que usted pueda intercambiarlo por diversos productos y servicios",
+        dt:"Nee, uw LATAM-paspunten worden omgedoopt tot Multiplus-punten, maar het saldo in uw account wordt niet beïnvloed door deze wijziging en zal beschikbaar zijn op het Multiplus-platform, zodat u het kunt omwisselen voor verschillende producten en services."
+    },
+    "position13":{
+        en:"Rescate",
+        pt:"Resgate",
+        es:"Rescue",
+        dt:"Redding"
+    },
+    "position14":{
+        en:"How will this change affect the airfare rescue process?",
+        pt:"Como essa mudança afetará no processo de resgatede passage aérea?",
+        es:"¿Cómo afectará este cambio en el proceso de rescate de pasajeros?",
+        dt:"Welke invloed heeft deze verandering op het reddingsproces van vliegtickets?"
+    },
+    "position15":{
+        en:"Will there be changes in the LATAM product catalog?",
+        pt:"Haverá mudanças no catálogo de produtos LATAM?",
+        es:"¿Habrá cambios en el catálogo de productos LATAM?",
+        dt:"Zullen er veranderingen zijn in de LATAM-productcatalogus?"
+    },
+    "position16":{
+        en:"Yes. With this change, you will have access to the Multiplus catalog where you can redeem your points for airfare.",
+        pt:"Sim. Com essa mudança, você terá acesso ao catálogo da Multiplus em que você poderá resgatar seus pontos  por passagens aéreas.",
+        es:"Sí. Con este cambio, usted tendrá acceso al catálogo de Multiplus en el que usted podrá rescatar sus puntos por pasajes aéreos.",
+        dt:"Ja, met deze wijziging hebt u toegang tot de Multiplus-catalogus, waar u uw punten kunt inwisselen voor vliegtickets."
+    },
+    "position17":{
+        en:"Accumulation",
+        pt:"Acúmulo",
+        es:"Acumulación",
+        dt:"Ophoping"
+    },
+    "position18":{
+        en:"What modifications will be made to the accumulation rules, flying with LATAM Airlines, oneworld airlines and associated airlines?",
+        pt:"Quais modificações serão feitas nas regras de acúmulo, voando com a LATAM Airlines,companhias aéreas oneworld e associadas?",
+        es:"¿Qué cambios se harán en las reglas de acumulación, volando con LATAM Airlines, compañías aéreas oneworld y asociadas?",
+        dt:"Welke wijzigingen worden aangebracht aan de accumulatieregels, vliegen met LATAM Airlines, oneworld airlines en geassocieerde luchtvaartmaatschappijen?"
+    },
+    "position19":{
+        en:"How to accumulate will not be affected. You will continue to accumulate under the same current rules, but your balance will be in Multiplus Points instead of LATAM Pass Miles.",
+        pt:"A forma de acumular não será afetada. Você continuará acumulando sob as mesmas regras atuais, mas seu saldo será em pontos Multiplus em vez de Milhas LATAM Pass.",
+        es:"La forma de acumular no se verá afectada. Usted continuará acumulando bajo las mismas reglas actuales, pero su saldo será en puntos Multiplus en lugar de Millas LATAM Pass.",
+        dt:"Hoe te accumuleren zal niet worden beïnvloed. Je zult blijven accumuleren volgens dezelfde huidige regels, maar je saldo zal in Multiplus Points zijn in plaats van LATAM Pass Miles."
+    },
+    "position20":{
+        en:"How will this change affect the rules of accumulation with financial institutions and partner companies?",
+        pt:"Como essa mudança afetará as regras de acúmulo com instituições financeiras e empresas parceiras?",
+        es:"Como ese cambio afectará las reglas de acumulación con instituciones financieras y empresas asociadas?",
+        dt:"Hoe zal deze verandering van invloed zijn op de regels van accumulatie met financiële instellingen en partnerbedrijven?"
+    },
+    "position21":{
+        en:"Starting in 2018, you will have access to new partnerships, which will allow you to have options to accumulate Multiplus points.",
+        pt:"A partir de 2018, você terá acesso a novas parcerias, o que permitirá que você tenha opções para acumular pontos Multiplus.",
+        es:"A partir de 2018, usted tendrá acceso a nuevas alianzas, lo que le permitirá tener opciones para acumular puntos Multiplus.",
+        dt:"Vanaf 2018 heeft u toegang tot nieuwe partnerschappen, waarmee u opties hebt om Multiplus-punten te verzamelen."
+    },
+    "position22":{
+        en:"Catheterization and Benefit",
+        pt:"Cateforia e Benefício",
+        es:"Cateforia y Beneficio",
+        dt:"Je zult snel lid worden van Multiplus!"
+    },
+    "position23":{
+        en:"What modifications will be made to the accumulation rules, flying with LATAM Airlines, oneworld airlines and associated airlines?",
+        pt:"Quais modificações serão feitas nas regras de acúmulo, voando com a LATAM Airlines,companhias aéreas oneworld e associadas?",
+        es:"¿Qué cambios se harán en las reglas de acumulación, volando con LATAM Airlines, compañías aéreas oneworld y asociadas?",
+        dt:"Catheterisatie en voordeel"
+    },
+    "position24":{
+        en:"How to accumulate will not be affected. You will continue to accumulate under the same current rules, but your balance will be in Multiplus Points instead of LATAM Pass Miles.",
+        pt:"A forma de acumular não será afetada. Você continuará acumulando sob as mesmas regras atuais, mas seu saldo será em pontos Multiplus em vez de Milhas LATAM Pass.",
+        es:"La forma de acumular no se verá afectada. Usted continuará acumulando bajo las mismas reglas actuales, pero su saldo será en puntos Multiplus en lugar de Millas LATAM Pass.",
+        dt:"Hoe te accumuleren zal niet worden beïnvloed. Je zult blijven accumuleren volgens dezelfde huidige regels, maar je saldo zal in Multiplus Points zijn in plaats van LATAM Pass Miles."
+    },
+    "position25":{
+        en:"How will this change affect the rules of accumulation with financial institutions and partner companies?",
+        pt:"Como essa mudança afetará as regras de acúmulo com instituições financeiras e empresas parceiras?",
+        es:"Como ese cambio afectará las reglas de acumulación con instituciones financieras y empresas asociadas?",
+        dt:"Hoe zal deze verandering van invloed zijn op de regels van accumulatie met financiële instellingen en partnerbedrijven?"
+    },
+    "position26":{
+        en:"Starting in 2018, you will have access to new partnerships, which will allow you to have options to accumulate Multiplus points.",
+        pt:"A partir de 2018, você terá acesso a novas parcerias, o que permitirá que você tenha opções para acumular pontos Multiplus.",
+        es:"A partir de 2018, usted tendrá acceso a nuevas alianzas, lo que le permitirá tener opciones para acumular puntos Multiplus.",
+        dt:"Vanaf 2018 heeft u toegang tot nieuwe partnerschappen, waarmee u opties hebt om Multiplus-punten te verzamelen."
+    },
+    "position27":{
+        en:"How will this change affect the rules of accumulation with financial institutions and partner companies?",
+        pt:"Como essa mudança afetará as regras de acúmulo com instituições financeiras e empresas parceiras?",
+        es:"Como ese cambio afectará las reglas de acumulación con instituciones financieras y empresas asociadas?",
+        dt:"Hoe zal deze verandering van invloed zijn op de regels van accumulatie met financiële instellingen en partnerbedrijven?"
+    },
+    "position28":{
+        en:"Starting in 2018, you will have access to new partnerships, which will allow you to have options to accumulate Multiplus points.",
+        pt:"A partir de 2018, você terá acesso a novas parcerias, o que permitirá que você tenha opções para acumular pontos Multiplus.",
+        es:"A partir de 2018, usted tendrá acceso a nuevas alianzas, lo que le permitirá tener opciones para acumular puntos Multiplus.",
+        dt:"Vanaf 2018 heeft u toegang tot nieuwe partnerschappen, waarmee u opties hebt om Multiplus-punten te verzamelen."
+    },
+    "position29":{
+        en:"You will continue to access the different routes operated by LATAM Airlines, oneworld airlines and associated airlines to fly to more than 1,000 destinations around the world.",
+        pt:"Você continuará tento acesso as diferentes rotas operadas pela LATAM Airlines, companhias aéreas oneworld  e associadas,  para voar para mais de 1.000destinos  ao redor do mundo.",
+        es:"Usted continuará intentando acceder a las diferentes rutas operadas por LATAM Airlines, compañías aéreas oneworld y asociadas, para volar a más de 1.000destinos alrededor del mundo.",
+        dt:"U blijft toegang houden tot de verschillende routes die door LATAM Airlines, oneworld airlines en geassocieerde luchtvaartmaatschappijen worden uitgevoerd om naar meer dan 1.000 bestemmingen over de hele wereld te vliegen."
+    },
+    "bolha-traslate1":{
+        en:"Soon, you will join Mutiplus",
+        pt:"Em breve, você se unirá à Mutiplus",
+        es:"En breve, usted se unirá a Mutiplus",
+        dt:"Binnenkort kom je bij Mutiplus"
+    },
+        "bolha-traslate2":{
+        en:"Take you further is worth more",
+        pt:"Levar<br> você mais longe vale mais",
+        es:"Llevar más lejos vale más",
+        dt:"Je verder brengen is meer waard"
+    },
+    "bolha-traslate4":{
+        en:"Multiplus and LATAM Airlines take you further",
+        pt:"A Multiplus e LATAM Airlines te levam mais longe",
+        es:"Multiplus y LATAM Airlines te llevan más lejos",
+        dt:"Multiplus en LATAM Airlines nemen u verder"
+    },
+    "bolha-traslate5":{
+        en:"Stay quiet,we love you explain everything",
+        pt:"Fique tranquilo, a gente te explica tudo",
+        es:"Quedarse tranquilo, la gente te lo explica todo",
+        dt:"Blijven rustig, we houden van je leg alles uit"
+    },
+    "bolha-tnm":{
+        en:"Take your doubts here",
+        pt:"Tire aqui suas dúvidas",
+        es:"Tira aquí tus dudas",
+        dt:"Neem je twijfels hier"
+    },
+    "positiones":{
+        en:"Balance",
+        pt:"Saldo",
+        es:"Equilibrio",
+        dt:"Balans"
+    },
+}
+
+var carrossel = function() {
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    nav:true,
+    items: 1,
+    dots:true,
+    responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+};
+var idioma = function() {
+    $('.box-flag').click(function() {
+        $('.content-flag').slideToggle('fast');
+        $(this).find('.btn-select-flag').toggleClass('btn-select-flag--open');
+    });
+    $('.content-flag .box-flag__text').click(function(){
+        var trocar = $(this).text();
+        var icon = $(this).attr('data-id');
+        var change = icon;
+        $('.flag-select .box-flag__text').text(trocar);
+        var icon = $('.flag-select .box-flag__text').attr('data-id', icon);
+        $('.content-flag .box-flag__text').removeClass('x-hide'); // removendo a classe e colocando ela de novo em outra posição 
+        $(this).addClass('x-hide');
+        var change = $('.bolha .bolha--change, .bolha .bolha--change1').attr('data-id',change);
+    });
+};
+ var pintar = function(){
+    $('.saldo-kit dt').click(function(){
+        $(this).toggleClass('novaazul');
+        $(this).next().slideToggle('fast');
+    });
+ };
+var traduzir = function(){
+    $('.content-flag .box-flag__text').click(function(){
+        var translate = $(this).attr('data-id').split('-')[1];
+        $('body').translate({lang: translate, t: tradutor});
+    });
+};
+$(document).ready(function() {
+    carrossel();
+    idioma();
+    pintar();   
+    traduzir();
+ });
